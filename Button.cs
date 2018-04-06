abd0c5ff417e94c388b2ade3002d3bcd27d62079
Nathan HANEN <hanen.nathan@gmail.com>
2018-04-06 16:51:10 +0200

    Possibility to set an icon to a button.
    Just set the texture of the icon in the Icon property.

--- Button.cs
+++ Button.cs
@@ -17,6 +17,11 @@ namespace WGP.Gui
     public class Button : Widget
     {
         private TEXT.Text Label { get; set; }
+        /// <summary>
+        /// Icon of the button.
+        /// </summary>
+        public Texture Icon { get; set; }
+        private RectangleShape iconRect;
         private RectangleShape Back { get; set; }
         private Vertex[] Border { get; set; }
         /// <summary>
@@ -39,6 +44,8 @@ namespace WGP.Gui
             Border = new Vertex[8];
 
             Label.Font = Init.Font;
+            Icon = null;
+            iconRect = new RectangleShape();
 
             InternUpdate();
         }
@@ -49,13 +56,25 @@ namespace WGP.Gui
             target.Draw(Back, new RenderStates(tr.Transform));
             target.Draw(Border, PrimitiveType.Lines, new RenderStates(tr.Transform));
             target.Draw(Label, new RenderStates(tr.Transform));
+            if (Icon != null)
+                target.Draw(iconRect, new RenderStates(tr.Transform));
         }
 
         internal override Vector2f GetMinimumSize()
         {
-            Vector2f result = new Vector2f(Label.FindCharacterPos((uint)Label.String.Count()).X, Init.TextSize);
+            Vector2f result = new Vector2f();
+            if (Icon == null || Label.String.Length > 0)
+            {
+                result = new Vector2f(Label.FindCharacterPos((uint)Label.String.Count()).X, Init.TextSize);
+                if (Icon != null)
+                    result.X += iconRect.Size.X;
+                result += new Vector2f(10, 10);
+            }
+            else
+            {
+                result = iconRect.Size;
+            }
             result += Padding * 2;
-            result += new Vector2f(20, 10);
             return result;
         }
 
@@ -63,6 +82,17 @@ namespace WGP.Gui
         {
             Back.Size = new Vector2f(ReservedSpace.Width, ReservedSpace.Height) - Padding * 2;
             Back.Position = new Vector2f(ReservedSpace.Left, ReservedSpace.Top) + Padding;
+            if (Icon != null)
+            {
+                float ratio = (float)Icon.Size.X / Icon.Size.Y;
+                iconRect.Size = new Vector2f((Init.TextSize + 10) * ratio, Init.TextSize + 10);
+                iconRect.Texture = Icon;
+                if (Label.String.Length > 0)
+                    iconRect.Position = Back.Position + new Vector2f(Back.Size.X / 2 - (iconRect.Size.X + 10 + Label.FindCharacterPos((uint)Label.String.Length).X) / 2,
+                                                            Back.Size.Y / 2 - iconRect.Size.Y / 2);
+                else
+                    iconRect.Position = Back.Position + Back.Size / 2 - iconRect.Size / 2;
+            }
             Border[0].Position = new Vector2f((int)ReservedSpace.Left + .5f, (int)ReservedSpace.Top + .5f) + Padding;
             Border[1].Position = Border[0].Position + new Vector2f((int)ReservedSpace.Width - Padding.X * 2, 0);
             Border[2].Position = Border[1].Position;
@@ -71,8 +101,13 @@ namespace WGP.Gui
             Border[5].Position = Border[4].Position + new Vector2f(-(int)ReservedSpace.Width + Padding.X * 2, 0);
             Border[6].Position = Border[5].Position;
             Border[7].Position = Border[6].Position + new Vector2f(0, -(int)ReservedSpace.Height + Padding.Y * 2);
-            Label.Position = new Vector2f((int)(ReservedSpace.Width / 2 - Label.FindCharacterPos((uint)Label.String.Count()).X / 2 + ReservedSpace.Left),
-                                          (int)(ReservedSpace.Height / 2 + ReservedSpace.Top - Init.TextSize / 2) + Init.TextSize);
+            {
+                float offset = 0;
+                if (Icon != null)
+                    offset = iconRect.Size.X;
+            Label.Position = new Vector2f((int)(Back.Size.X / 2 - (Label.FindCharacterPos((uint)Label.String.Length).X + offset) / 2 + Back.Position.X + offset),
+                                          (int)(Back.Size.Y / 2 - Init.TextSize / 2 + Init.TextSize + Back.Position.Y));
+            }
 
             if (Pressed)
             {
